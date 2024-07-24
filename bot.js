00e015de2a2f0eedabefbd1cdc59cdc8c1b11768
torn42 <zhabkapepe5@gmail.com>
2024-07-24 15:28:21 +0500

    added bot server

---
+++ bot.js
@@ -0,0 +1,31 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+// Замените 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота
+const token = '7389501710:AAFJhZRg2-rp25ewRwFZr3sc9mdeMUuPL5g';
+
+// Замените 'YOUR_APP_URL' на URL вашего приложения
+const appUrl = 'https://t.me/fronendinternshipbot/salestrucktestapp';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const message = 'Добро пожаловать! Нажмите на кнопку ниже, чтобы открыть веб-приложение.';
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Открыть вебку',
+            url: appUrl
+          }
+        ]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, message, options);
+});
+
+console.log('Бот запущен...');
